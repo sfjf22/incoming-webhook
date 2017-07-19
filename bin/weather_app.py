af4aec463a9e76c9a16e2c6561b028a77b173745
John Fung <sf.joejones1@gmail.com>
2017-07-18 20:47:09 -0700

    Added weather_app.py

---
+++ bin/weather_app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import json
+import sys
+import urllib2
+
+if __name__ == '__main__':
+
+    # Check command line arguments
+    if len(sys.argv) != 4:
+        print "Invalid number of arguments!"
+        print "Usage   : ", sys.argv[0], " <wunderground API key> <State> <City>"
+        print "Example : ", sys.argv[0], " 123456789 GA San_Francisco"
+        sys.exit(1)
+
+    # Initialize
+    apikey = sys.argv[1]
+    state  = sys.argv[2]
+    city   = sys.argv[3]
+
+    # Get weather info from 'api.wunderground.com'
+    url = 'http://api.wunderground.com/api/' + apikey + '/conditions/q/' + state + '/' + city + '.json'
+    response = urllib2.urlopen(url).read()
+
+    # Convert data to Json
+    data = json.loads(response)
+
+    # Retrieve today's predicted precipitation
+    precip_today_metric = data['current_observation']['precip_today_metric']
+
+    # Send a message to Slack Channel if the predicted precipitation is > 0
+    if int(precip_today_metric) > 0:
+        # Slack channel url
+        url = 'https://hooks.slack.com/services/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'
+        message = 'Predicted precipitation of ' + str(precip_today_metric) + ' mm today at ' + city + ", " + state
+        data = json.dumps({'text': message})
+
+        # Send a HTTP request
+        req  = urllib2.Request(url, data, {'Content-Type': 'application/json'})
+        f = urllib2.urlopen(req)
+        response = f.read()
+        print response
+        f.close()
+
